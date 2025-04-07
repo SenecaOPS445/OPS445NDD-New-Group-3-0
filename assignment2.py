443cb1103869a57e0ab438a8b30112339bd9036b
Prince Patel <pspatel53@myseneca.ca>
2025-04-07 15:44:41 -0400

    Added calculate_cpu_usage function

--- assignment2.py
+++ assignment2.py
@@ -16,3 +16,23 @@ def snapshot_cpu():
     for value in parts[1:]:
         cpu_times.append(int(value))
     return cpu_times
+
+def calculate_cpu_usage(interval):
+    
+    cpu_start = snapshot_cpu()
+    total_start = sum(cpu_start)
+    idle_start = cpu_start[3]  
+
+    time.sleep(interval)
+
+    cpu_end = snapshot_cpu()
+    total_end = sum(cpu_end)
+    idle_end = cpu_end[3]
+
+    total_diff = total_end - total_start
+    idle_diff = idle_end - idle_start
+
+    if total_diff == 0:
+        return 0
+    usage = (total_diff - idle_diff) / total_diff * 100
+    return usage
