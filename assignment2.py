ca01d698a0a9cbe803bec6aa2c7b83e27a085c44
Prince Patel <pspatel53@myseneca.ca>
2025-04-07 15:50:12 -0400

    Documented snapshot_memory function

--- assignment2.py
+++ assignment2.py
@@ -53,14 +53,23 @@ def calculate_cpu_usage(interval):
     return usage
 
 def snapshot_memory():
+    """
+
+    Read memory usage information from /proc/meminfo.
+
+    This function creates a dictionary containing memory info values,
+    such as total and available memory.
+    """
 
     meminfo = {}
     file = open("/proc/meminfo", "r")
     for line in file:
         parts = line.split(":")
         if len(parts) >= 2:
-            key = parts[0].strip()               
-            value = parts[1].strip().split()[0]  
+            key = parts[0].strip()               # e.g., "MemTotal", "MemAvailable"
+            value = parts[1].strip().split()[0]  # Get the first number
             meminfo[key] = int(value)
     file.close()
     return meminfo
+
+
