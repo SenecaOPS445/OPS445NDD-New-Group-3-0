dcd2c27f2d210064b6155490ca979b59689c56f3
Prince Patel <pspatel53@myseneca.ca>
2025-04-07 15:54:33 -0400

    Documented calculate_memory_usage function

--- assignment2.py
+++ assignment2.py
@@ -73,12 +73,17 @@ def snapshot_memory():
     return meminfo
 
 def calculate_memory_usage():
+    """
+    Calculate memory usage as a percentage.
+                    
+    The usage is determined by subtracting available memory from total memory."
+    """
+    memory = snapshot_memory()
+    if "MemTotal" in memory and "MemAvailable" in memory:
+        total = memory["MemTotal"]
+        available = memory["MemAvailable"]
+        used = total - available
+        usage = used / total * 100
+        return usage
+    return 0
 
-        memory = snapshot_memory()
-        if "MemTotal" in memory and "MemAvailable" in memory:
-            total = memory["MemTotal"]
-            available = memory["MemAvailable"]
-            used = total - available
-            usage = used / total * 100
-            return usage
-        return 0
